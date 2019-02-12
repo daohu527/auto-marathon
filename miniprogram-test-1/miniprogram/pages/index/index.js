cc299de6eabc3f0f4885715b056694da7c2633ac
daohu527 <daohu527@gmail.com>
2019-02-12 09:29:54 +0800

    add

---
+++ miniprogram-test-1/miniprogram/pages/index/index.js
@@ -0,0 +1,27 @@
+//index.js
+const app = getApp()
+
+Page({
+  axis: [
+    {
+      time: '2018-2-15',
+      name: '张三',
+      event: '垃圾太多'
+    },
+    {
+      time: '2018-2-15',
+      name: '王三',
+      event: '垃圾太多'
+    },
+    {
+      time: '2018-2-15',
+      name: '张三',
+      event: '垃圾太多'
+    },
+    {
+      time: '2018-2-15',
+      name: '张三',
+      event: '垃圾太多'
+    },
+  ]
+})
